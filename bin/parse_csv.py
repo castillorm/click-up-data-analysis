75c63012b7526a69a8f2be3f2975312151e565f0
Rod Castillo <rod.castillo@me.com>
2023-04-25 15:48:17 -0400

    Adding the initial code that we had worked on.

---
+++ bin/parse_csv.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# 1. Open the .csv file using Pandas into a DF
+# dataframe = pd.read_csv('../data/addresses.csv')
+dataframe = pd.read_csv('../data/my_clickup_data.csv')
+print(dataframe)
+# 2. Create a dataset that includes addresses from SD
+# rslt_df = dataframe[dataframe['state'] == 'SD'] 
+# rslt_df = dataframe[dataframe["state"].str.contains("SD",case=False)]
+# 3. Write the dataset to a datafile
+# print(rslt_df)
+# rslt_df.to_csv('../output.csv', header=False, index=False)
+
+
+# 2. Create a dataset that includes only the columns "Task Name" and "Assignee"
+# rslt_df = dataframe[dataframe['state'] == 'SD'] 
+rslt_df = dataframe[['Task Name', 'Assignee']]
+# print(rslt_df)
+# rslt_df = dataframe[dataframe["state"].str.contains("SD",case=False)]
+
+# 3. Create a dataset that only includes tasks that "Smith" was assigned
+rslt_df = dataframe[dataframe["Assignee"].str.contains("Smith",case=False)]
+print(rslt_df)
+
+# 3. Write the dataset to a datafile
+# print(rslt_df)
+# rslt_df.to_csv('../output.csv', header=False, index=False)
